a152507229b7245a99fd93a9cbc77f1efd775443
ToraKaji <ogradysjr@gmail.com>
2018-11-10 16:00:57 -0600

    Added a method to check for wins. Only horizontal wins established.

--- app/src/main/java/deepdive/cnm/edu/pennies/view/fragments/Game0.java
+++ app/src/main/java/deepdive/cnm/edu/pennies/view/fragments/Game0.java
@@ -3,20 +3,20 @@ package deepdive.cnm.edu.pennies.view.fragments;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import de.hdodenhof.circleimageview.CircleImageView;
 import deepdive.cnm.edu.pennies.R;
+import deepdive.cnm.edu.pennies.model.Button;
 import deepdive.cnm.edu.pennies.model.Scratcher;
 import deepdive.cnm.edu.pennies.model.ScratcherDB;
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
@@ -36,70 +36,83 @@ public class Game0 extends Fragment {
   private CircleImageView[] button = new CircleImageView[idButtonArray.length];
   private TextView[] textVal = new TextView[idTextArray.length];
 
-  //Creates a random object
-  private SecureRandom rng = new SecureRandom();
-
+  //Creates an array of all known values
+  int[] values = {-1,-2,-3,-4,-5,-6,-7,-8,-9};
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     View view = inflater.inflate(R.layout.fragment_game0, container, false);
-    for(int i = 0;i<idButtonArray.length;i++){
-      createButton(view,idButtonArray[i],i,idTextArray[i]);
+    for (int i = 0; i < idButtonArray.length; i++) {
+      button[i] = createButton(view, i, idButtonArray[i], idTextArray[i]);
     }
 
     return view;
   }
 
 
-
-
-
   private class InsertTask extends AsyncTask<Scratcher, Void, Long> {
 
     @Override
     protected Long doInBackground(Scratcher... scratchers) {
+      Log.d("TEST", "Added entity entry");
       return ScratcherDB.getInstance(getContext()).getScratcherDao().insert(scratchers[0]);
+
     }
-
-  }
-
-  private boolean checkWin() {
-
-    return false;
   }
 
 
-  private CircleImageView createButton(View view, int id, final int num, final int textId) {
+  private CircleImageView createButton(View view, final int num, int id,  final int textId) {
 
+    //Creates a RNG for the values of 0-50 in increments of 10.
     Random rng = new Random();
-
     final List<Integer> possible = new ArrayList<>();
     for (int i = 0; i <= 51; i += 10) {
       possible.add(i);
     }
-    Collections.shuffle(possible);
 
-    final int value = possible.get(rng.nextInt(possible.size()-1));
+    //Creates fields to set listeners and textview objects
+    final int value = possible.get(rng.nextInt(possible.size() - 1));
     final TextView textX = view.findViewById(textId);
     final CircleImageView buttonX = view.findViewById(id);
 
+    //Sets onClickListeners for each button created.
     buttonX.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
+        //Sets up the entity class for DB.
         Scratcher scratch = new Scratcher();
         scratch.setId(num);
         scratch.setDate(new Date());
         scratch.setValue(value);
         new InsertTask().execute(scratch);
+        //Physical effects on the object.
         buttonX.setVisibility(View.INVISIBLE);
         textX.setText(String.valueOf(value));
         textX.setVisibility(View.VISIBLE);
+        values[num] = value;
+        checkWin();
         Toast.makeText(getContext(), "I'm button: " + num, Toast.LENGTH_SHORT).show();
       }
     });
-
     return buttonX;
+  }
+
+private void checkWin() {
+    boolean win = false;
+    for(int x = 0;x<7;x+=3) {
+      //Checks for horizontal wins ( x==x+1&&x==x+2 == true linear horizontal win.)
+      if (values[x] == values[x + 1] && values[x] == values[x + 2]) {
+        win = true;
+      } else {
+        win = false;
+      }
+    }
+    //TODO handle other wins
+    if(win==true){
+      Toast.makeText(getActivity(), "You won!", Toast.LENGTH_SHORT).show();
+    }
+
   }
 }
